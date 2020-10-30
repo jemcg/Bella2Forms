bd302f9099385471746cb7f00650dae74bee440d
Jaime McGovern <jaimemcgovern@outlook.com>
2020-10-30 04:01:36 +0000

    First Commit

---
+++ Bella2Forms/Bella2Forms/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace Bella2Forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cb_Gluten = new System.Windows.Forms.CheckBox();
+            this.cb_Crustaceans = new System.Windows.Forms.CheckBox();
+            this.cb_Eggs = new System.Windows.Forms.CheckBox();
+            this.cb_Fish = new System.Windows.Forms.CheckBox();
+            this.cb_Peanuts = new System.Windows.Forms.CheckBox();
+            this.cb_Soybeans = new System.Windows.Forms.CheckBox();
+            this.cb_Milk = new System.Windows.Forms.CheckBox();
+            this.cb_TreeNuts = new System.Windows.Forms.CheckBox();
+            this.cb_Celery = new System.Windows.Forms.CheckBox();
+            this.cb_Mustard = new System.Windows.Forms.CheckBox();
+            this.cb_Sesame = new System.Windows.Forms.CheckBox();
+            this.cb_Sulphites = new System.Windows.Forms.CheckBox();
+            this.cb_Lupin = new System.Windows.Forms.CheckBox();
+            this.cb_Molluscs = new System.Windows.Forms.CheckBox();
+            this.btn_GoToDishes = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select all allergens (Max 4)";
+            // 
+            // cb_Gluten
+            // 
+            this.cb_Gluten.AutoSize = true;
+            this.cb_Gluten.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cb_Gluten.Location = new System.Drawing.Point(15, 78);
+            this.cb_Gluten.Name = "cb_Gluten";
+            this.cb_Gluten.Size = new System.Drawing.Size(54, 17);
+            this.cb_Gluten.TabIndex = 1;
+            this.cb_Gluten.Text = "Gluten";
+            this.cb_Gluten.UseVisualStyleBackColor = true;
+            // 
+            // cb_Crustaceans
+            // 
+            this.cb_Crustaceans.AutoSize = true;
+            this.cb_Crustaceans.Location = new System.Drawing.Point(15, 117);
+            this.cb_Crustaceans.Name = "cb_Crustaceans";
+            this.cb_Crustaceans.Size = new System.Drawing.Size(85, 17);
+            this.cb_Crustaceans.TabIndex = 2;
+            this.cb_Crustaceans.Text = "Crustaceans";
+            this.cb_Crustaceans.UseVisualStyleBackColor = true;
+            // 
+            // cb_Eggs
+            // 
+            this.cb_Eggs.AutoSize = true;
+            this.cb_Eggs.Location = new System.Drawing.Point(15, 158);
+            this.cb_Eggs.Name = "cb_Eggs";
+            this.cb_Eggs.Size = new System.Drawing.Size(50, 17);
+            this.cb_Eggs.TabIndex = 3;
+            this.cb_Eggs.Text = "Eggs";
+            this.cb_Eggs.UseVisualStyleBackColor = true;
+            // 
+            // cb_Fish
+            // 
+            this.cb_Fish.AutoSize = true;
+            this.cb_Fish.Location = new System.Drawing.Point(15, 200);
+            this.cb_Fish.Name = "cb_Fish";
+            this.cb_Fish.Size = new System.Drawing.Size(45, 17);
+            this.cb_Fish.TabIndex = 4;
+            this.cb_Fish.Text = "Fish";
+            this.cb_Fish.UseVisualStyleBackColor = true;
+            // 
+            // cb_Peanuts
+            // 
+            this.cb_Peanuts.AutoSize = true;
+            this.cb_Peanuts.Location = new System.Drawing.Point(15, 246);
+            this.cb_Peanuts.Name = "cb_Peanuts";
+            this.cb_Peanuts.Size = new System.Drawing.Size(65, 17);
+            this.cb_Peanuts.TabIndex = 5;
+            this.cb_Peanuts.Text = "Peanuts";
+            this.cb_Peanuts.UseVisualStyleBackColor = true;
+            // 
+            // cb_Soybeans
+            // 
+            this.cb_Soybeans.AutoSize = true;
+            this.cb_Soybeans.Location = new System.Drawing.Point(15, 290);
+            this.cb_Soybeans.Name = "cb_Soybeans";
+            this.cb_Soybeans.Size = new System.Drawing.Size(73, 17);
+            this.cb_Soybeans.TabIndex = 6;
+            this.cb_Soybeans.Text = "Soybeans";
+            this.cb_Soybeans.UseVisualStyleBackColor = true;
+            // 
+            // cb_Milk
+            // 
+            this.cb_Milk.AutoSize = true;
+            this.cb_Milk.Location = new System.Drawing.Point(15, 338);
+            this.cb_Milk.Name = "cb_Milk";
+            this.cb_Milk.Size = new System.Drawing.Size(45, 17);
+            this.cb_Milk.TabIndex = 7;
+            this.cb_Milk.Text = "Milk";
+            this.cb_Milk.UseVisualStyleBackColor = true;
+            // 
+            // cb_TreeNuts
+            // 
+            this.cb_TreeNuts.AutoSize = true;
+            this.cb_TreeNuts.Location = new System.Drawing.Point(382, 78);
+            this.cb_TreeNuts.Name = "cb_TreeNuts";
+            this.cb_TreeNuts.Size = new System.Drawing.Size(73, 17);
+            this.cb_TreeNuts.TabIndex = 8;
+            this.cb_TreeNuts.Text = "Tree Nuts";
+            this.cb_TreeNuts.UseVisualStyleBackColor = true;
+            // 
+            // cb_Celery
+            // 
+            this.cb_Celery.AutoSize = true;
+            this.cb_Celery.Location = new System.Drawing.Point(382, 117);
+            this.cb_Celery.Name = "cb_Celery";
+            this.cb_Celery.Size = new System.Drawing.Size(55, 17);
+            this.cb_Celery.TabIndex = 9;
+            this.cb_Celery.Text = "Celery";
+            this.cb_Celery.UseVisualStyleBackColor = true;
+            // 
+            // cb_Mustard
+            // 
+            this.cb_Mustard.AutoSize = true;
+            this.cb_Mustard.Location = new System.Drawing.Point(382, 158);
+            this.cb_Mustard.Name = "cb_Mustard";
+            this.cb_Mustard.Size = new System.Drawing.Size(64, 17);
+            this.cb_Mustard.TabIndex = 10;
+            this.cb_Mustard.Text = "Mustard";
+            this.cb_Mustard.UseVisualStyleBackColor = true;
+            // 
+            // cb_Sesame
+            // 
+            this.cb_Sesame.AutoSize = true;
+            this.cb_Sesame.Location = new System.Drawing.Point(382, 200);
+            this.cb_Sesame.Name = "cb_Sesame";
+            this.cb_Sesame.Size = new System.Drawing.Size(64, 17);
+            this.cb_Sesame.TabIndex = 11;
+            this.cb_Sesame.Text = "Sesame";
+            this.cb_Sesame.UseVisualStyleBackColor = true;
+            // 
+            // cb_Sulphites
+            // 
+            this.cb_Sulphites.AutoSize = true;
+            this.cb_Sulphites.Location = new System.Drawing.Point(382, 246);
+            this.cb_Sulphites.Name = "cb_Sulphites";
+            this.cb_Sulphites.Size = new System.Drawing.Size(69, 17);
+            this.cb_Sulphites.TabIndex = 12;
+            this.cb_Sulphites.Text = "Sulphites";
+            this.cb_Sulphites.UseVisualStyleBackColor = true;
+            // 
+            // cb_Lupin
+            // 
+            this.cb_Lupin.AutoSize = true;
+            this.cb_Lupin.Location = new System.Drawing.Point(382, 290);
+            this.cb_Lupin.Name = "cb_Lupin";
+            this.cb_Lupin.Size = new System.Drawing.Size(52, 17);
+            this.cb_Lupin.TabIndex = 13;
+            this.cb_Lupin.Text = "Lupin";
+            this.cb_Lupin.UseVisualStyleBackColor = true;
+            // 
+            // cb_Molluscs
+            // 
+            this.cb_Molluscs.AutoSize = true;
+            this.cb_Molluscs.Location = new System.Drawing.Point(382, 338);
+            this.cb_Molluscs.Name = "cb_Molluscs";
+            this.cb_Molluscs.Size = new System.Drawing.Size(67, 17);
+            this.cb_Molluscs.TabIndex = 14;
+            this.cb_Molluscs.Text = "Molluscs";
+            this.cb_Molluscs.UseVisualStyleBackColor = true;
+            // 
+            // btn_GoToDishes
+            // 
+            this.btn_GoToDishes.Location = new System.Drawing.Point(639, 365);
+            this.btn_GoToDishes.Name = "btn_GoToDishes";
+            this.btn_GoToDishes.Size = new System.Drawing.Size(102, 37);
+            this.btn_GoToDishes.TabIndex = 15;
+            this.btn_GoToDishes.Text = "Go To Dishes";
+            this.btn_GoToDishes.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_GoToDishes);
+            this.Controls.Add(this.cb_Molluscs);
+            this.Controls.Add(this.cb_Lupin);
+            this.Controls.Add(this.cb_Sulphites);
+            this.Controls.Add(this.cb_Sesame);
+            this.Controls.Add(this.cb_Mustard);
+            this.Controls.Add(this.cb_Celery);
+            this.Controls.Add(this.cb_TreeNuts);
+            this.Controls.Add(this.cb_Milk);
+            this.Controls.Add(this.cb_Soybeans);
+            this.Controls.Add(this.cb_Peanuts);
+            this.Controls.Add(this.cb_Fish);
+            this.Controls.Add(this.cb_Eggs);
+            this.Controls.Add(this.cb_Crustaceans);
+            this.Controls.Add(this.cb_Gluten);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "BellaAllergens";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox cb_Gluten;
+        private System.Windows.Forms.CheckBox cb_Crustaceans;
+        private System.Windows.Forms.CheckBox cb_Eggs;
+        private System.Windows.Forms.CheckBox cb_Fish;
+        private System.Windows.Forms.CheckBox cb_Peanuts;
+        private System.Windows.Forms.CheckBox cb_Soybeans;
+        private System.Windows.Forms.CheckBox cb_Milk;
+        private System.Windows.Forms.CheckBox cb_TreeNuts;
+        private System.Windows.Forms.CheckBox cb_Celery;
+        private System.Windows.Forms.CheckBox cb_Mustard;
+        private System.Windows.Forms.CheckBox cb_Sesame;
+        private System.Windows.Forms.CheckBox cb_Sulphites;
+        private System.Windows.Forms.CheckBox cb_Lupin;
+        private System.Windows.Forms.CheckBox cb_Molluscs;
+        private System.Windows.Forms.Button btn_GoToDishes;
+    }
+}
+
